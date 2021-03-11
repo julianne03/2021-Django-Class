d26d03b6cb189bf9cd25dbd065d5e0f01d55d657
s2019w33@e-mirim.hs.kr <s2019w33@e-mirim.hs.kr>
2021-03-11 09:57:27 +0900

    olls/models was_published_recently, __str__

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,15 +1,28 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    # 그날그날 최신 글
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    def __str__(self):
+        return self.question_text
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
 
 # 1. models.py 변경하기
 # 2. makemigrations 를 통해 변경사항에 대한 마이그레이션을 만들기
